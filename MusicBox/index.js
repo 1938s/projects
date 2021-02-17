3d1d3ce404dbea817f8fc439a09448a873bc0d4b
1938s <ifbkbzsexgirl@gmail.com>
2021-02-17 13:34:27 +0800

    修改目录结构

---
+++ MusicBox/index.js
@@ -0,0 +1,247 @@
+'use strict';
+
+let musicList = [];
+let playStatus = false;
+
+let inputFile = document.getElementById('inputFile');
+let audio = document.getElementById('audio');
+let loopOne = document.getElementById('loopOne');
+let loopOrder = document.getElementById('loopOrder');
+let loopRandom = document.getElementById('loopRandom');
+let searchInput = document.getElementById('music-search-input');
+let musicPlayerForm = document.querySelector('form');
+let musicPlayer = document.querySelector('.music-player');
+let musicListEl = document.querySelector('.music-list');
+let musicTotal = document.querySelector('.music-total');
+let musicCurrent = document.querySelector('.music-current');
+let musicPlayerBtn = document.querySelector('.music-player-btn');
+let musicProgressBar = document.querySelector('.music-progress-bar');
+
+inputFile.addEventListener('change', ev => {
+    getFiles(ev);
+});
+audio.addEventListener('play', play);
+audio.addEventListener('pause', pause);
+audio.addEventListener('timeupdate', ev => {
+    audioPercent(ev);
+});
+musicPlayerForm.addEventListener('submit', ev => {
+    ev.preventDefault();
+    search();
+});
+document.body.addEventListener('click', ev => {
+    let musicSearchList = document.querySelector('.music-search-list');
+    if (ev.target.className !== 'search-item' && musicSearchList) {
+        musicSearchList.style.display = 'none';
+        searchInput.value = '';
+    }
+});
+
+function getFiles (e) {
+    musicTotal.style.display = 'inline-block';
+    musicPlayer.style.display = 'block';
+    musicTotal.innerHTML = e.target.files.length + ' files.';
+
+
+    for (const file of e.target.files) {
+        let path = URL.createObjectURL(file);
+        let el = document.createElement('div');
+        musicList.unshift({
+            name: file.name,
+            path: path
+        });
+
+        el.innerText = file.name;
+        el.style.cssText = 'cursor: pointer;line-height: 2rem;font-size: 1.3rem;';
+        mouseHover(el, path);
+        el.addEventListener('click', () => {
+            document.title = file.name;
+            musicCurrent.innerHTML = file.name;
+            audio.src = path;
+            playMode('play');
+            setMusicColor(el);
+            audio.addEventListener('ended', this.pause);
+        });
+        musicListEl.append(el);
+    }
+}
+
+function loopMode (mode) {
+    if (audio) {
+        if (mode === 'one') {
+            audio.loop = true;
+            setLoopColor('loopOne');
+        } else if (mode === 'order') {
+            audio.loop = false;
+            setLoopColor('loopOrder');
+            next();
+            audio.addEventListener('ended', next);
+        } else {
+            audio.loop = false;
+            setLoopColor('loopRandom');
+            random();
+            audio.addEventListener('ended', this.random);
+        }
+    }
+}
+
+function next () {
+    setMusicColor('next');
+    let musicItem = musicList.pop();
+    let path = musicItem.path;
+
+    document.title = musicItem.name;
+    musicCurrent.innerHTML = musicItem.name;
+    audio.src = path;
+    musicList.unshift({
+        name: musicItem.name,
+        path: path
+    });
+    play();
+}
+
+function random () {
+    let index = Math.floor(Math.random() * musicList.length);
+    setMusicColor('random', index);
+
+    let musicItem = musicList[index];
+    let path = musicItem.path;
+
+    document.title = musicItem.name;
+    musicCurrent.innerHTML = musicItem.name;
+    audio.src = path;
+    musicList.unshift({
+        name: musicItem.name,
+        path: path
+    });
+    play();
+}
+
+function setLoopColor (mode) {
+    if (mode === 'loopOne') {
+        loopOne.style.color = 'red';
+        loopOrder.style.color = 'black';
+        loopRandom.style.color = 'black';
+    } else if (mode === 'loopOrder') {
+        loopOne.style.color = 'black';
+        loopOrder.style.color = 'red';
+        loopRandom.style.color = 'black';
+    } else {
+        loopOne.style.color = 'black';
+        loopOrder.style.color = 'black';
+        loopRandom.style.color = 'red';
+    }
+}
+
+function audioPercent (ev) {
+    let musicProgressBar = document.querySelector('.music-progress-bar');
+    let musicProgress = document.querySelector('.music-progress');
+    let musicProgressBarLength = +(getComputedStyle(musicProgressBar).width.split('px')[0]);
+    if (ev.target.duration) {
+        let musicProgressPercent = ev.target.currentTime / ev.target.duration * musicProgressBarLength;
+        musicProgress.style.width = musicProgressPercent + 'px';
+    }
+}
+
+function search () {
+    if (searchInput.value) {
+        let musicSearchList = document.querySelector('.music-search-list');
+        musicSearchList.innerHTML = '';
+        musicSearchList.style.display = 'block';
+        musicList.forEach((it, index) => {
+            let div = document.createElement('div');
+            if (it.name.includes(searchInput.value)) {
+                div.innerText = it.name;
+                div.className = 'search-item';
+                div.style.cssText = 'line-height: 1.5rem; cursor: pointer';
+                mouseHover(div);
+                div.addEventListener('click', () => {
+                    audio.src = musicList[index].path;
+                    play();
+                    setMusicColor('search', index);
+                    searchInput.value = '';
+                    musicCurrent.innerHTML = it.name;
+                    document.title = it.name;
+                    musicSearchList.style.display = 'none';
+                });
+            }
+            musicSearchList.append(div);
+        });
+    }
+}
+
+function playMode (mode) {
+    if (audio && audio.src) {
+        if (mode === 'play') {
+            play();
+        } else {
+            playStatus ? pause() : play();
+        }
+    } else {
+        loopMode('order');
+    }
+}
+
+function clickInput () {
+    inputFile.click();
+}
+
+function play () {
+    audio.play();
+    playStatus = true;
+    musicPlayerBtn.innerHTML = 'Pause';
+    musicProgressBar.style.display = 'block';
+}
+
+function pause () {
+    audio.pause();
+    playStatus = false;
+    musicPlayerBtn.innerHTML = 'Play';
+}
+
+function mouseHover (div, path) {
+    div.addEventListener('mouseover', () => {
+        div.style.color = 'red';
+    });
+    div.addEventListener('mouseleave', () => {
+        if (path && path === audio.src) {
+            div.style.color = 'red';
+        } else {
+            div.style.color = 'black';
+        }
+    });
+}
+
+function setMusicColor (el, index) {
+    if (el === 'next') {
+        musicListEl.childNodes.forEach(it => {
+            let index = musicList.length;
+            if (it.innerText !== musicList[index - 1].name) {
+                it.style.color = 'black';
+            } else {
+                it.style.color = 'red';
+            }
+        });
+    } else if (el === 'random') {
+        musicListEl.childNodes.forEach(it => {
+            if (it.innerText !== musicList[index].name) {
+                it.style.color = 'black';
+            } else {
+                it.style.color = 'red';
+            }
+        });
+    } else if (el === 'search') {
+        musicListEl.childNodes.forEach(it => {
+            if (it.innerText !== musicList[index].name) {
+                it.style.color = 'black';
+            } else {
+                it.style.color = 'red';
+            }
+        });
+    } else {
+        musicListEl.childNodes.forEach(it => {
+            it.style.color = 'black';
+        });
+        el.style.color = 'red';
+    }
+}
---
+++ TextEditor/index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+let flag = false;
+// const imgUrl = "url(./IU.png)";
+const placeholder = 'Note Everything ...';
+const eventObj= {
+    'beforeunload': beforeunload,
+    'pagehide': beforeunload,
+    'load': load,
+    'pageshow': load,
+    'drop': drop,
+    'keydown': keydown
+};
+
+init();
+
+function init () {
+    for (const evKey in eventObj) {
+        window.addEventListener(evKey, eventObj[evKey]);
+    }
+
+    document.body.contentEditable = true;
+    document.body.style.cssText =
+        `font-size: 1.5rem;
+             line-height: 1.5;
+             width: 75%;
+             margin: 0 auto;
+             padding: 4rem;
+             color: black;`
+    // background: center / cover fixed no-repeat ${imgUrl};
+}
+
+function beforeunload () {
+    !flag
+        ? localStorage.setItem('content', document.body.innerHTML)
+        : localStorage.setItem('content', '');
+}
+
+function load () {
+    let content = localStorage.getItem('content');
+
+    flag = true;
+
+    if (!content) {
+        document.body.innerHTML = placeholder;
+        window.addEventListener('click', e => {
+            e.target.innerHTML === placeholder && (e.target.innerHTML = '');
+            flag = false;
+        });
+    } else {
+        document.body.innerHTML = content;
+        flag = false;
+    }
+}
+
+function drop (e) {
+    e.preventDefault();
+
+    !localStorage.getItem('content').length
+    && (document.body.innerHTML === placeholder)
+    && (document.body.innerHTML = '');
+
+    let file = new FileReader();
+    file.readAsDataURL(e.dataTransfer.files[0]);
+
+    file.onload = function () {
+        let img = document.createElement('img');
+        img.src = file.result;
+        document.body.appendChild(img);
+    };
+}
+
+function keydown (e) {
+    if (e.key === "Tab") {
+        fontColor(e, 1);
+    } else if ((e.metaKey || e.ctrlKey) && (e.key === 'r')) {
+        fontColor(e, 0, 'red');
+    } else if ((e.metaKey || e.ctrlKey) && (e.key === 'b')) {
+        fontColor(e, 0, 'black');
+    } else {
+        return true;
+    }
+}
+
+function fontColor (e, offset, color) {
+    e.preventDefault();
+    let range = e.view.getSelection().getRangeAt(0);
+    let div = document.createElement('div');
+
+    div.style.cssText = 'vertical-align: text-top;display: inline-block;';
+
+    if (color) {
+        div.innerHTML = '&nbsp';
+        div.style.color = color;
+    } else {
+        div.innerHTML = '&nbsp &nbsp &nbsp ';
+    }
+
+    e.view.getSelection().removeAllRanges();
+    e.view.getSelection().addRange(range);
+
+    range.insertNode(div);
+    range.setStart(div, offset);
+    range.setEnd(div, 1);
+}
